940d22f708f42cf2ec77a2a62c415f85d6ec5416
897800754@qq.com <897800754@qq.com>
2023-07-25 15:44:51 +0800

    adjust

--- design/src/main/java/design/v2/architectural/facade/demo1/DwarvenGoldmineFacade.java
+++ design/src/main/java/design/v2/architectural/facade/demo1/DwarvenGoldmineFacade.java
@@ -1,7 +1,7 @@
 package design.v2.architectural.facade.demo1;
 
-import com.sun.tools.javac.util.List;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -9,29 +9,32 @@ import java.util.Collection;
  */
 public class DwarvenGoldmineFacade {
 
-  private final List<DwarvenMineWorker> workers;
+    private final ArrayList<DwarvenMineWorker> workers;
 
-  public DwarvenGoldmineFacade() {
-      workers = List.of(
-            new DwarvenGoldDigger(),
-            new DwarvenCartOperator(),
-            new DwarvenTunnelDigger());
-  }
+    public DwarvenGoldmineFacade() {
+        workers = new ArrayList<DwarvenMineWorker>() {
+            {
+                this.add(new DwarvenGoldDigger());
+                this.add(new DwarvenCartOperator());
+                this.add(new DwarvenTunnelDigger());
+            }
+        };
+    }
 
-  public void startNewDay() {
-    makeActions(workers, DwarvenMineWorker.Action.WAKE_UP, DwarvenMineWorker.Action.GO_TO_MINE);
-  }
+    public void startNewDay() {
+        makeActions(workers, DwarvenMineWorker.Action.WAKE_UP, DwarvenMineWorker.Action.GO_TO_MINE);
+    }
 
-  public void digOutGold() {
-    makeActions(workers, DwarvenMineWorker.Action.WORK);
-  }
+    public void digOutGold() {
+        makeActions(workers, DwarvenMineWorker.Action.WORK);
+    }
 
-  public void endDay() {
-    makeActions(workers, DwarvenMineWorker.Action.GO_HOME, DwarvenMineWorker.Action.GO_TO_SLEEP);
-  }
+    public void endDay() {
+        makeActions(workers, DwarvenMineWorker.Action.GO_HOME, DwarvenMineWorker.Action.GO_TO_SLEEP);
+    }
 
-  private static void makeActions(Collection<DwarvenMineWorker> workers,
-                                  DwarvenMineWorker.Action... actions) {
-    workers.forEach(worker -> worker.action(actions));
-  }
+    private static void makeActions(Collection<DwarvenMineWorker> workers,
+                                    DwarvenMineWorker.Action... actions) {
+        workers.forEach(worker -> worker.action(actions));
+    }
 }
